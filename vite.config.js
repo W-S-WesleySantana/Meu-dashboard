6bcc6ccc99ab36101abe40f9bdf29a36848d5115
WS-WesleySantana <wesleymemo7@gmail.com>
2026-09-16 22:09:53 -0300

    Configurando deploy automatico do Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Meu-dashboard/', // 👈 ADICIONE ESSA LINHA
+  base: '/Meu-dashboard/',
 })
